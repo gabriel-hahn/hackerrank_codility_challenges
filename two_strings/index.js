30abb1e650b4e2ada87af7cbb2faa1c6cf36369d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-22 09:30:15 -0300

    Two strings added

---
+++ two_strings/index.js
@@ -0,0 +1,12 @@
+/**
+ * This challenge is about identify when one or more characteres exists in the other string.
+ * For example, 'tea' and 'mouse' shares the same charactere 'e', but 'fork' and 'cat' don't have any
+ * characteres in common.
+ * 
+ * https://www.hackerrank.com/challenges/two-strings/problem
+ */
+
+// Complete the twoStrings function below.
+function twoStrings(s1, s2) {
+  return s1.split('').find(c => s2.includes(c)) ? 'YES' : 'NO';
+}
