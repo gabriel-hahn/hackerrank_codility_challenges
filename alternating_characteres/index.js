c09e523297501d5d9b52f6620b9e2b04b1e5308e
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-22 09:18:57 -0300

    Alterning Characteres added

---
+++ alternating_characteres/index.js
@@ -0,0 +1,28 @@
+/**
+ * I just check if the array is only one kind of character, e.g AAA or BBB validating it.
+ * As the second validation, I go trought the string removing the repeated characteres and returning
+ * the correct length in the end.
+ * 
+ * https://www.hackerrank.com/challenges/alternating-characters/problem
+ */
+
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+  const onlyOneKindCharacter = s.match("^(.)\\1*$"); // AAA or BBB
+
+  if (onlyOneKindCharacter) {
+    return s.length - 1;
+  }
+
+  const finalStr = s.split('').reduce((sumResult, c) => {
+    if (sumResult) {
+      const lastCharacter = sumResult[sumResult.length - 1];
+
+      return lastCharacter === c ? sumResult : sumResult.concat(c);
+    }
+
+    return c;
+  });
+
+  return s.length - finalStr.length;
+}
